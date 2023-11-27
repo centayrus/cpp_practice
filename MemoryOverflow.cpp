d124367c2b0f11627ef5bf2682ba62fbd6b3a651
Artem.Tamiev <centayrus@gmail.com>
2023-11-27 17:35:47 +0500

    new any commit

--- MemoryOverflow.cpp
+++ MemoryOverflow.cpp
@@ -2,29 +2,20 @@
 #include <iostream>
 #include <limits>
 #include <string>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-    const auto min = numeric_limits<int64_t>::min();     // минимальное значение типа int64_t
-    const auto max = numeric_limits<uint64_t>::max(); // максимальное значение типа uint64_t
-    cout << min << endl;
-    cout << max << endl;
+    const auto min = numeric_limits<int64_t>::min;     // минимальное значение типа int64_t
+    const auto max = numeric_limits<uint64_t>::max; // максимальное значение типа uint64_t
+    cout << "min = "s << min << endl;
+    cout << "max = "s << max << endl;
     auto sum = min + max;
-<<<<<<< HEAD
-  //  auto min2 = (min);
-  //  auto max2 = 2*(max);
-    cout << sum << endl;
-  //  cout << min2 << endl;
-   // cout << max2 << endl;
-=======
-    auto min2 = 2*(min);
-    auto max2 = 2*(max);
-    cout << sum << endl;
-    cout << min2 << endl;
-    cout << max2 << endl;
->>>>>>> main
+    auto min2 = min*2;
+    auto max2 = max*2; 
+    cout << "min + max = "s << min + max << endl;
+    cout << "min*2 = "s << min2 << endl;
+    cout << "max*2 = "s << max2 << endl;
 
     // выведите 5 чисел
 }
