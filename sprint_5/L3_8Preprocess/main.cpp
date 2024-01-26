3e8a72fe4baedc3e7feb7fac554ae65b0eec9030
Artem.Tamiyev <artem.tamiyev@nexign.com>
2024-01-26 16:37:48 +0500

    почти доделанный код по препроцессору

--- sprint_5/L3_8Preprocess/main.cpp
+++ sprint_5/L3_8Preprocess/main.cpp
@@ -14,70 +14,83 @@ using filesystem::path;
 path operator""_p(const char *data, std::size_t sz) {
     return path(data, data + sz);
 }
-path LocalDirSearch(const path &parent, const path &inc_path) {
-    path p3;
-    for (const auto &dir_entry : filesystem::directory_iterator(parent / inc_path.parent_path())) {
+path LocalDirSearch(const path &parent, const path& inc_path) {
+    error_code err;
+    path find_path, empty_path;
+    for (const auto &dir_entry : filesystem::directory_iterator(parent / inc_path.parent_path(), err )) { // 
         path p = inc_path.filename().string();
-       // path p2 = parent / inc_path;
         if (dir_entry.path().filename().string() == p && dir_entry.status().type() == filesystem::file_type::regular) {
-            ifstream in_check(dir_entry.path());
-            p3 = dir_entry.path();
-            return p3;
-           // Preprocess(parent / inc_path, out_file, in_file, include_directories);
-            //return true;
+                find_path = dir_entry.path();
+               ifstream in_check(dir_entry.path());
+               if (in_check) {
+                 return find_path;
+               }
         }
     }
-    return p3;
+    return empty_path;
 }
 
-path VectorDirSearch(const path &inc_path, const vector<path> &include_directories, const path in_file) {
-    path p3;
+path VectorDirSearch(const path &inc_path, const vector<path> &include_directories) {
+    error_code err;
+    path find_path, empty_path;
     for (const auto &v_path : include_directories) {
-        for (const auto &dir_entry : filesystem::directory_iterator(v_path / inc_path.parent_path())) {
-            if (dir_entry.path().filename().string() == inc_path.filename().string() && dir_entry.status().type() == filesystem::file_type::regular) {
+        for (const auto &dir_entry : filesystem::directory_iterator(v_path / inc_path.parent_path(), err)) { // 
+            if (dir_entry.path().filename().string() == inc_path.filename().string() && dir_entry.status().type() == filesystem::file_type::regular) {             
+                find_path = dir_entry.path();
+        //        cout << find_path << endl;
               ifstream in_check(dir_entry.path());
-            p3 = dir_entry.path();
-            return p3;
-           // Preprocess(parent / inc_path, out_file, in_file, include_directories);
-            //return true;
+              if (in_check) {
+                return find_path;
+              }
         }
         }
     }
-    cout << "unknown include file "s << inc_path.filename().string() << " at file "s << in_file << " at line "s << __LINE__ + 1 << endl;
-    return p3;
+    return empty_path;
 }
 
 bool Preprocess(const path &in_file, const path &out_file, const path &cur_file, const vector<path> &include_directories) {
     smatch m;
     string str;
-    path parent = in_file.parent_path();
+  //  cout << "in_file: "s << in_file << endl;
+   path parent = in_file.parent_path();
     static const regex includ_file(R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
     static const regex includ_lib(R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
     ifstream in(in_file);
-    while (!in) {
-        continue;
-    }
+  //  while (!in) {
+  //      continue;
+   // }
     ofstream out(out_file, ios::app);
     while (getline(in, str)) {
         path inc_path;
         if (regex_match(str, m, includ_file)) {
             inc_path = string(m[1]);
-            path p = LocalDirSearch(parent, inc_path);
-
-            if (!p.empty()) {
-                Preprocess(p, out_file, in_file, include_directories);
+            path local_path = LocalDirSearch(parent, inc_path);
+            if (!local_path.empty()) {
+                Preprocess(parent / inc_path, out_file, in_file, include_directories);
+                continue;
             } else {
-                path p2 = VectorDirSearch(inc_path, include_directories, in_file);
-                if (!p2.empty()) {
-                    Preprocess(p2, out_file, in_file, include_directories);
+                path vector_path = VectorDirSearch(inc_path, include_directories);
+                if (!vector_path.empty()) {
+                    Preprocess(vector_path, out_file, in_file, include_directories);
+                    continue;
                 }
             }
+        cout << "unknown include file "s << inc_path.filename().string() << " at file "s << cur_file.string() << " at line "s << __LINE__ << endl;
+        return false;
+
         } else if (regex_match(str, m, includ_lib)) {
             inc_path = string(m[1]);
+            path vector_path = VectorDirSearch(inc_path, include_directories);
+          //  cout << "vector_path: "s << vector_path << endl;
+            if (!vector_path.empty()) {
+                Preprocess(vector_path, out_file, in_file, include_directories);
+                    continue;
+            }
+        cout << "unknown include file "s << inc_path.filename().string() << " at file "s << cur_file.string() << " at line "s << __LINE__ << endl; 
+        return false;  
         }
-        if (!inc_path.empty()) {
-            VectorDirSearch(inc_path, include_directories, in_file);
-        }
+        
+
         out << str << endl;
     }
     return true;
@@ -88,7 +101,7 @@ bool Preprocess(const path &in_file, const path &out_file, const vector<path> &i
     if (!in) {
         return false;
     }
-    path p = in_file.filename().string();
+  //  path p = in_file.filename().string();
     return Preprocess(in_file, out_file, in_file, include_directories);
 }
 
@@ -144,10 +157,10 @@ void Test() {
         ofstream file("sources/include2/lib/std2.h");
         file << "// std2\n"s;
     }
-    Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "include1"_p, "sources"_p / "include2"_p});
-    //                      {"sources"_p / "include1"_p, "sources"_p / "include2"_p})
-    //  assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-    //                      {"sources"_p / "include1"_p, "sources"_p / "include2"_p})));
+    // Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p, {"sources"_p / "include1"_p, "sources"_p / "include2"_p});
+    //                      {"sources"_p / "include1"_p, "sources"_p / "include2"_p});
+     assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
+                         {"sources"_p / "include1"_p, "sources"_p / "include2"_p})));
 
     ostringstream test_out;
     test_out << "// this comment before include\n"
@@ -163,8 +176,8 @@ void Test() {
                 "\n"
                 "int SayHello() {\n"
                 "    cout << \"hello, world!\" << endl;\n"s;
-    // GetFileContents("sources/a.in"s);
-    //  assert(GetFileContents("sources/a.in"s) == test_out.str());
+    GetFileContents("sources/a.in"s);
+     assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
 
 int main() {
