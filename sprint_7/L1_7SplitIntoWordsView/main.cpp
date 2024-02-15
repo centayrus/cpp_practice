2cde146ec69fa87c503fb43ea2bb54f9bda31123
Artem.Tamiev <centayrus@gmail.com>
2024-02-15 23:27:26 +0500

    sprint 7 in work

--- sprint_7/L1_7SplitIntoWordsView/main.cpp
+++ sprint_7/L1_7SplitIntoWordsView/main.cpp
@@ -7,25 +7,20 @@ using namespace std;
 
 vector<string_view> SplitIntoWordsView(string_view str) {
     vector<string_view> result;
-    //int64_t pos = str.find_first_not_of(" ");
     str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
-    //const int64_t pos_end = str.npos;
-    
-    while (str.size() > 0) {
-        int64_t space = str.find(' ');
-       // result.push_back(space == str.npos ? str.substr(0, pos_end) : str.substr(0, space));
 
-       result.push_back(str.substr(0, space));
-        //pos = str.find_first_not_of(" ", space);
-        str.remove_prefix(std::min(str.find_first_not_of(" "), str.size()));
+    while (!str.empty()) {
+        int64_t space = str.find(' ');
+        result.push_back(str.find(' ') == str.npos ? str.substr(0, str.npos) : str.substr(0, str.find(' ')));
+        str.remove_prefix(std::min(str.find_first_not_of(" ", space), str.size()));
     }
 
     return result;
 }
 
 int main() {
-    // assert((SplitIntoWordsView("") == vector<string_view>{}));
-    // assert((SplitIntoWordsView("     ") == vector<string_view>{}));
+    assert((SplitIntoWordsView("") == vector<string_view>{}));
+    assert((SplitIntoWordsView("     ") == vector<string_view>{}));
     assert((SplitIntoWordsView("   aaaaaaa") == vector{"aaaaaaa"sv}));
     assert((SplitIntoWordsView("a") == vector{"a"sv}));
     assert((SplitIntoWordsView("a b c") == vector{"a"sv, "b"sv, "c"sv}));
